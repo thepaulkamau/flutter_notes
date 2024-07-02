59beb912cd915d503d51ef0c4fbd56c46c1d5e4b
thepaulkamau <thepaulkamau@gmail.com>
2024-07-03 00:56:49 +0300

    First Flutter app on Git.  Creating note, viewing and deleting. Finished and working.

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notes/models/note_model.dart';
+import 'package:flutter_notes/screens/create_note.dart';
+import 'package:flutter_notes/screens/widgets/note_card.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomeScreen> {
+
+  List<Note> notes = List.empty(growable: true);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ANVIL PAUL Notes"),
+      ),
+
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, index){
+          return NoteCard(note: notes[index], index: index, onNoteDeleted:onNoteDeleted);
+        },
+      ), 
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            Navigator.of(context).push(MaterialPageRoute(builder: (context)=> CreateNote(onNewNoteCreated: onNewNoteCreated,)));
+
+          },
+           child: const Icon(Icons.add)
+           ),
+
+    );
+  }     
+
+  void onNewNoteCreated(Note note){
+    notes.add(note);
+    setState(() {});
+
+  }
+
+  void onNoteDeleted(int index){
+    notes.removeAt(index);
+    setState(() {});
+  }
+}
